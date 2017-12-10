38b3e438c549ceaeb3a02b2117c6315bba8bf7f7
Josh Vanderwillik <joshwillik@gmail.com>
2017-12-09 22:12:12 -0500

    Align todos with headers

--- src/public/js/nav.jsx
+++ src/public/js/nav.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {NavLink} from 'react-router-dom'
 
 export default () => (
-  <div>
+  <div className="col-sm">
     <NavLink to="/" className="nav_link" title="Todos">
       <i className="fa fa-2x fa-check-square"/></NavLink>
     <NavLink to="/games/chess" className="nav_link" title="Dan's chess stats">
