052f3f6dad8e079f7b5fcaaa5008b883ff880b39
Daniel <dbaldesarra9@gmail.com>
2017-12-09 21:40:45 -0500

    done-list

--- src/public/js/app.jsx
+++ src/public/js/app.jsx
@@ -1,62 +1,77 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import ListItem from './list-item.jsx'
 import api_request from './api.js'
 
 export default class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
-    this.state = {todos: []}
+    this.state = { todos: [] }
     this.get_todos = this.get_todos.bind(this)
     this.on_change = this.on_change.bind(this)
     this.on_submit = this.on_submit.bind(this)
     this.todo_modified = this.todo_modified.bind(this)
   }
-  get_todos () {
+  get_todos() {
     api_request('/todos')
-    .then(res => {
-      return res.json()
-    }).then(todos => {
-      this.setState({todos: todos})
-    }).catch(err => {
-      console.error(err)
-    })
+      .then(res => {
+        return res.json()
+      }).then(todos => {
+        this.setState({ todos: todos })
+      }).catch(err => {
+        console.error(err)
+      })
   }
-  componentDidMount () {
+  componentDidMount() {
     this.get_todos()
   }
-  todo_modified (updated) {
+  todo_modified(updated) {
     let todos = this.state.todos.map(todo => {
       return todo._id === updated._id ? updated : todo
     })
-    this.setState({todos})
+    this.setState({ todos })
   }
-  on_change (event) {
-    this.setState({value: event.target.value})
+  on_change(event) {
+    this.setState({ value: event.target.value })
   }
-  on_submit (event) {
+  on_submit(event) {
     event.preventDefault()
     this.refs.todo_input.value = ''
     api_request('/todos', {
       method: 'POST',
-      body: {title: this.state.value, completed: false},
+      body: { title: this.state.value, completed: false },
     }).then(() => {
       this.get_todos()
     }).catch(err => {
       console.error(err)
     })
   }
-  render () {
+  render() {
+    let [completed, in_progress] = _.partition(this.state.todos, todo => todo.completed)
     return (
       <div>
         <h1>To do</h1>
         <form onSubmit={this.on_submit} className="new_todo">
           <input ref='todo_input' placeholder='Add a todo'
-            onChange={this.on_change} autoFocus={true}/>
+            onChange={this.on_change} autoFocus={true} />
           <button className="primary">&#43;</button>
         </form>
         <ul className="todo_items">
-        {this.state.todos.map(todo =>
-          <ListItem modified={this.todo_modified} key={todo._id} todo={todo}/>)}
+          {in_progress.map(todo =>
+            <ListItem
+              modified={this.todo_modified}
+              key={todo._id}
+              disabled={todo.completed}
+              todo={todo} />)}
+        </ul>
+
+        <h1>Done</h1>
+        <ul className="todo_items completed_todos">
+          {completed.map(todo =>
+            <ListItem
+              modified={this.todo_modified}
+              key={todo._id}
+              disabled={todo.completed}
+              todo={todo} />)}
         </ul>
       </div>
     )
