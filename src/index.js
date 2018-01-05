cd8826706cdc7b4412a2520175659c508b696986
Josh Vanderwillik <joshwillik@gmail.com>
2018-01-04 21:58:51 -0500

    Remove chess stats backend

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const util = require('./route_handlers/util')
 const session = require('express-session')
 const compression = require('compression')
 const todos = require('./route_handlers/todos')
-const chess = require('./route_handlers/lichess')
 
 let app = express()
 app.use(compression())
@@ -27,14 +26,14 @@ app.use(passport.session())
 if (config.auto_login) {
   app.use(util.auto_login)
 }
-app.use(body_parser.urlencoded({ extended: false }))
+app.use(body_parser.urlencoded({extended: false}))
 app.use(body_parser.json())
 app.post('/signup', user.sign_up)
 app.get('/login', auth.require_no_auth({ otherwise: '/' }),
   files.send_file('login.html'))
 app.post('/login', passport.authenticate('local'), util.redirect('/'))
 app.get('/logout', user.logout)
-app.use(auth.require_auth({ otherwise: '/login' }))
+app.use(auth.require_auth({otherwise: '/login'}))
 app.get('/auth/wunderlist', passport.authenticate('wunderlist'))
 app.get('/auth/wunderlist/callback',
   passport.authenticate('wunderlist'),
@@ -58,8 +57,6 @@ app.post('/api/todos', todos.add)
 app.get('/api/todos/:id', todos.get)
 app.patch('/api/todos/:id', todos.update)
 app.delete('/api/todos/:id', todos.remove)
-app.get('/games/chess', files.send_file('index.html'))
-app.get('/api/lichess', chess.fetch_user)
 app.listen(config.http_port,
   () => console.log(`Listening on 0.0.0.0:${config.http_port}`))
 init().catch(err => console.error(err))
--- src/lichess.js
+++
@@ -1,9 +0,0 @@
-const rq = require('request-promise')
-const config = require('./config')
-let E = module.exports
-
-E.fetch_user = () => {
-  return rq({
-    url: `https://lichess.org/api/user/${config.lichess_id}`,
-  }).catch(err => console.error(e))
-}
